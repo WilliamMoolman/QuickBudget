6f96d5b32a294c494ff7ee8b4c55c55dc9d02d2c
William Moolman <moolman.william@gmail.com>
2023-11-06 12:45:56 +0000

    feat(frontend): add account filter

--- src/frontend/pages/transactions.py
+++ src/frontend/pages/transactions.py
@@ -18,10 +18,14 @@ df['date'] = pd.to_datetime(df['date'], errors='coerce')
 
 # Filtering data
 st.sidebar.title("Filter")
+st.sidebar.subheader("Time filter")
 filter_type = st.sidebar.radio('Filter Type', ['Month', 'Year', 'None'])
 months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 month = st.sidebar.selectbox('Month', months, disabled=filter_type != 'Month')
 year = st.sidebar.number_input('Year', 1900, 2100, 2021, disabled=filter_type == 'None')
+
+st.sidebar.subheader("Account filter")
+account = st.sidebar.selectbox('Account', ['All']+sorted(df['account'].unique()), index=0)
 
 if filter_type == 'None':
     filtered_df = df
@@ -29,6 +33,9 @@ elif filter_type == 'Month':
     filtered_df = df[(df['date'].dt.month == months.index(month)+1) & (df['date'].dt.year == year)]
 else:
     filtered_df = df[df['date'].dt.year == year]
+
+if account != 'All':
+    filtered_df = filtered_df[filtered_df['account'] == account]
 
 display_df = filtered_df[['date','description','amount','account','category','notes']]
 
